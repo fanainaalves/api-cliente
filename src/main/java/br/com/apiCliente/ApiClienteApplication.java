a48b1e05cffbf12d7926041065521ad471079b36
Fanaina Alves <fanaiina.allves@gmail.com>
2024-03-05 15:45:09 -0300

    Criando o projeto

---
+++ src/main/java/br/com/apiCliente/ApiClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.apiCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apiCliente/services/CreateClientServiceTest.java
@@ -0,0 +1,8 @@
+package br.com.apiCliente.services;
+
+import org.junit.jupiter.api.Test;
+
+public class CreateClientServiceTest {
+
+    @Test
+}
